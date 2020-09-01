9604c1748c48160e96aca7cf47568bac452f13bb
May Lawver <may@maycod.es>
2020-08-31 21:48:22 -0400

    Add targetable input + styling

--- src/app/battlefield-region/battlefield-region.component.ts
+++ src/app/battlefield-region/battlefield-region.component.ts
@@ -4,11 +4,15 @@ import { BattlefieldRegion } from '../classes/BattlefieldRegion';
 @Component({
   selector: 'app-battlefield-region',
   templateUrl: './battlefield-region.component.html',
+  host: {
+    '[class.targetable]': 'targetable'
+  },
   styleUrls: ['./battlefield-region.component.css']
 })
 export class BattlefieldRegionComponent implements OnInit {
 
   @Input() region: BattlefieldRegion;
+  @Input() targetable: boolean
 
   constructor() { }
 
--- src/app/unit/unit.component.ts
+++ src/app/unit/unit.component.ts
@@ -4,11 +4,15 @@ import { Unit } from '../classes/Unit';
 @Component({
   selector: 'app-unit',
   templateUrl: './unit.component.html',
+  host: {
+    '[class.targetable]': 'targetable'
+  },
   styleUrls: ['./unit.component.css']
 })
 export class UnitComponent implements OnInit {
 
   @Input() unit: Unit;
+  @Input() targetable: boolean;
 
   constructor() { }
 
