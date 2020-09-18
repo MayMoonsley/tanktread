8a934d0ff5d7523e768a5e1252b2c4a25ffd4a00
May Lawver <may@maycod.es>
2020-09-18 15:34:39 -0400

    Add nodule

--- src/app/classes/Resource.ts
+++ src/app/classes/Resource.ts
@@ -9,6 +9,7 @@ export class Resource {
     public static readonly Cordylith = new Resource('Cordylith', 'Crystalline fungus mesh. Mood regulator in low doses.', 20);
     public static readonly Hide = new Resource('Hide', 'Miscellaneous creature dermis. Usable after processing.', 1);
     public static readonly Gristle = new Resource('Gristle', 'Low-quality creature flesh. Technically edible.', 1);
+    public static readonly Nodule = new Resource('Nodule', 'Sucrose storage organ. Calorie dense.', 10);
 
     private constructor(public readonly name: string, public readonly description: string, public readonly value: number) {}
 
