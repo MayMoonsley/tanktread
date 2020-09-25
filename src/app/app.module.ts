b9fabf6d96033928e1c7fd182bffa4aeded8442c
May Lawver <may@maycod.es>
2020-09-25 00:41:29 -0400

    Put units in accordions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { InventoryScreenComponent } from './inventory-screen/inventory-screen.co
 import { FormatInfinityPipe } from './format-infinity.pipe';
 import { UnitBuildScreenComponent } from './unit-build-screen/unit-build-screen.component';
 import { DefaultMessagePipe } from './default-message.pipe';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
+import { AccordionModule } from 'primeng/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
     declarations: [
@@ -30,6 +33,8 @@ import {ButtonModule} from 'primeng/button';
     imports: [
         BrowserModule,
         ButtonModule,
+        AccordionModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/classes/Unit.ts
+++ src/app/classes/Unit.ts
@@ -4,6 +4,7 @@ import { Status } from './Status';
 import { Targetable } from '../interfaces/Targetable';
 import { Arrays } from '../util/Arrays';
 import { ResourceDrop, resourceDropToAmount, ResourceInventory, Resource } from './Resource';
+import { ɵbypassSanitizationTrustResourceUrl } from '@angular/core';
 
 export enum UnitFaction {
     Tank = '👤', Drone = '🤖', Creature = '🐛'
@@ -37,6 +38,14 @@ export class Unit implements Targetable {
         for (const status of statuses) {
             this.addStatus(status);
         }
+    }
+
+    get formattedName(): string {
+        let r: string = `${this.faction} ${this.name}`;
+        if (this.statuses.length > 0) {
+            r += ` ${this.statuses.map(status => status.emoji).join('')}`;
+        }
+        return r;
     }
 
     get playerControlled(): boolean {
