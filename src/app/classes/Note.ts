3fde066b20d4cd44772d6db4a01b72d94540ab7d
May Lawver <may@maycod.es>
2020-11-19 13:12:57 -0500

    Add prelim tutorial stuff

--- src/app/classes/Note.ts
+++ src/app/classes/Note.ts
@@ -8,10 +8,10 @@ export class Note {
 
     Please be aware that you are responsible for both your pre-existing 500&curren; debt and the additional cost of your tank, gene therapies, life insurance, etc. In total, your debt amounts to **5000&curren;** and must be paid in full before you may retire from service.
 
-    In order to comply with Committee regulations and prevent unnecessary losses, you have been given the following informative literature:
+    In order to comply with Committee regulations and prevent unnecessary losses, you have been given the following informative literature (abridged to save space):
 
     * _Tank Combat for the Debt-Ridden_
-    * _History and Nature of the Blight (Abridged Fourth Edition)_
+    * _History and Nature of the Blight (Fourth Edition)_
     * _Cities, the Committee, and You_
 
     We are all wishing you a meritorious term as pilot!
@@ -25,9 +25,18 @@ export class Note {
     );
 
     public static readonly Combat = new Note('Tank Combat for the Debt-Ridden',
-    ``);
+    `
+    Hello, pilot! I'm sure you're going through a lot right now. Your tank is a state-of-the-art amalgam of machinery and software, but it doesn't make the melding process any easier. Stressed pilots aren't productive pilots, so take your time to recuperate. This guide is here to help you be an effective hunter once the post-gene therapy migraine subsides.
 
-    public static readonly Blight = new Note('History and Nature of the Blight (Abridged Fourth Edition)',
+    Historically, most losses came from pilots having to make difficult decisions under time pressure. This problem has been solved by an experimental blend of software and synthetic neurotransmitters. Combat has been abstracted to a clear interface, and it will feel as though you have infinite time to decide what to do. Information on entities, such as nature, physical integrity, and time left to act are determined through extensive machine analysis and displayed with friendly icons.
+
+    Your tank's extensive armor renders it too unwieldy to engage in combat directly. To counteract this, each tank is outfitted with drone-manufacturing capabilities. Each drone requires a certain amount of material to build; the tank produces a certain amount each "turn." Material is represented with a ⚙️ icon; the sidebar allows you to spend said material on units. New schematics can frequently be bought when transacting with cities.
+
+    Drones can be commanded and your tank can be piloted via buttons representing their capabilities. Targetable regions or entities will show up in green and can be selected by clicking on them, and you can cancel a command with the X button next to it.
+
+    Resources in a particular region will be listed at the bottom. They can be collected by your tank and some drones and sold in cities.`);
+
+    public static readonly Blight = new Note('History and Nature of the Blight (Fourth Edition)',
     ``);
 
     public static readonly Cities = new Note('Cities, the Committee, and You',
